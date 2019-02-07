087e165ee025b56e37658662f366d972e86fdfa4
Steve Mustanski <steve.mustanski@gmail.com>
2019-02-06 21:54:19 -0500

    added test for game and cleaned up linting issues

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,7 +5,7 @@ class Game extends Component {
   render() {
     return (
       <div>
-        <h3>Play Nine</h3>
+        <h3 id="heading">Play Nine</h3>
       </div>
     )
   }
--- src/components/__tests__/Game.test.js
+++ src/components/__tests__/Game.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Game from '../Game';
 import { shallow } from 'enzyme';
 
 describe('Game', function () {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Game />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    shallow(<Game />);
   });
 
+  it('displays the game heading', () => {
+    const mountedGame = shallow(<Game />);
+    const GameHeading = mountedGame.find('#heading');
+    expect(GameHeading.text()).toEqual('Play Nine');
+  })
 })
