2bfb6ef259bd9ced8da9effbd030aa48a7f1fc9b
Steve Mustanski <steve.mustanski@gmail.com>
2019-02-08 12:29:52 -0500

    added tests and logic for displaying 1 to 9 stars in the stars component

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Stars extends Component {
 
-  render() {
+const Stars = ()  => {
+
+  const numberOfStars = Math.floor(Math.random() * 9) + 1;
+  
+  let stars = [];
+  for (let i = 0; i < numberOfStars; i++ ) {
+    stars.push(<i key={i} className="fa fa-star"></i>)
+  }
+
     return (
       <div className="col-5">
-        Stars
+        {stars}
       </div>
     )
-  }
 }
 
 export default Stars;
--- src/components/__tests__/Stars.test.js
+++ src/components/__tests__/Stars.test.js
@@ -12,4 +12,10 @@ describe('Stars', function (){
     const col = mountedStars.find('.col-5');
     expect(col.length).toBe(1);
   })
+
+  it('shows between 1 and 9 stars', () => {
+    const star = mountedStars.find('.fa-star');
+    expect(star.length).toBeGreaterThanOrEqual(1);
+    expect(star.length).toBeLessThanOrEqual(9);
+  })
 })
