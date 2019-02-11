0a33ee350dbc0696f56075d088db5356768ad270
Steve Mustanski <steve.mustanski@gmail.com>
2019-02-10 22:34:52 -0500

    word change and refactor test in Answer

--- src/components/__tests__/Answer.test.js
+++ src/components/__tests__/Answer.test.js
@@ -3,7 +3,7 @@ import Answer from '../Answer';
 import { shallow } from 'enzyme';
 
 describe('Answer', function (){
-  let selectedNumbers = [2,3];
+  let selectedNumbers = [2,4];
   const mountedAnswer = shallow(<Answer selectedNumbers={selectedNumbers}/>);
 
   it('renders without crashing', () => {
@@ -13,16 +13,10 @@ describe('Answer', function (){
     const col = mountedAnswer.find('.col-5');
     expect(col.length).toBe(1);
   })
-
-  it.skip('displays the number that is passed to it', () => {
-    const number = mountedAnswer.find('span');
-    expect(number.text()).toEqual('2');
-  })
-
-  it('displays the number that is passed to it', () => {
+  it('displays the numbers that are passed to it', () => {
     const number = mountedAnswer.find('span');
     expect(number.at(0).text()).toEqual('2');
-    expect(number.at(1).text()).toEqual('3');
+    expect(number.at(1).text()).toEqual('4');
   })
 })
 
